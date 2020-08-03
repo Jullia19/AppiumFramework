60f01b00b62dc7deacff75b2b5a9470d673a4e96
Julia Soroko <jullia.a.k@gmail.com>
2020-08-03 13:38:34 +0300

    update version of maven plugin 2

--- src/main/java/practise/base.java
+++ src/main/java/practise/base.java
@@ -33,7 +33,7 @@ public class base {
 		{
 			service= AppiumDriverLocalService
 					.buildService(new AppiumServiceBuilder()
-							.usingDriverExecutable(new File("C:/Users/jullia/AppData/Roaming/npm/node_modules/appium/node_modules"))
+							.usingDriverExecutable(new File("C:/Program Files/nodejs"))
 							.withAppiumJS(
 									new File(
 											"C:/Users/jullia/AppData/Roaming/npm/node_modules/appium/lib/main.js"))
